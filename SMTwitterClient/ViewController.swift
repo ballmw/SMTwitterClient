b6c33293bb52b3828bb1a3c0cb158f5fb65b09e1
Michael Ball <mwball@gmail.com>
2014-08-26 17:39:24 -0400

    initial commit

---
+++ SMTwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMTwitterClient
+//
+//  Created by Michael Ball on 8/26/14.
+//  Copyright (c) 2014 Source Main LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
